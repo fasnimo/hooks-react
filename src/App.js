4351808649e75d8b50ec30a87bdc5f2645732a48
fasnimo <fasnimo@gmail.com>
2021-02-07 15:32:25 -0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,40 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState, useEffect } from 'react';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = ({ login }) => {
+  const [data, setData] = useState(null)
+  const [loading, setLoading] = useState(false)
+  const [error, setError] = useState(null)
+  
+  useEffect(() => {
+    if(!login) return;
+      setLoading(true)
+      fetch(`https://api.github.com/users/${login}`)
+      .then(response => response.json())
+      .then(setData)
+      .then(() => setLoading(false))
+      .catch(setError);
+  }, [login]) //dependicy array
+
+  if(loading) return <h1>Loading...</h1>;
+  if(error) 
+    return <pre>{JSON.stringify(error, null, 2)}</pre>;
+  if(!data) return null;
+
+  if(data){
+    debugger
+    return (
+      <div>
+        <h1>{data.name}</h1>
+        <h2>Username: {data.login}</h2>
+        <img alt={data.login} src={data.avatar_url} />
+        <p>URL: {data.url}</p>
+        <p>Followers: {data.followers}</p>
+        <p>Followers URL: {data.followers_url}</p>
+        <p>Following: {data.following}</p>
+        <p>Following URL: {data.following_url}</p>
+      </div>
+    );
+  };
+};
 
 export default App;
